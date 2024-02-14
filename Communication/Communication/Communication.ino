58f2dd6035a0d72dd807354a4ca9ea306ac439b7
sKulll-J <43633082+sKulll-J@users.noreply.github.com>
2024-02-14 20:05:05 +0100

    ma communication

--- Communication/Communication/Communication.ino
+++ Communication/Communication/Communication.ino
@@ -1,39 +1,58 @@
 
 #include <SoftwareSerial.h>
 
-SoftwareSerial gpsSerial(6, 7); // RX, TX
+#define maxCnt 64
+#define RX_PIN 2
+#define TX_PIN 7
 
-char buffer[64];
+SoftwareSerial gpsSerial(RX_PIN, TX_PIN); // RX, TX
+
+char buffer[maxCnt];
 int count=0;
+
+
+void handleRxInterrupt();
 
 void setup() {
   // put your setup code here, to run once:
     gpsSerial.begin(9600);                 // the gpsSerial baud rate
     Serial.begin(9600);   
+    attachInterrupt(digitalPinToInterrupt(RX_PIN), handleRxInterrupt, RISING); //CHANGE - RISING - FALLING
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-    if (gpsSerial.available()) 
-    {                   // if date is coming from software serial port ==> data is coming from gpsSerial shield   
-        while(gpsSerial.available()) {             // reading data into char array
-              buffer[count++]=gpsSerial.read();    // writing data into array
-              if(count == 64)break;
-        }
+    /*if (gpsSerial.available()) 
+    {                   // if date is coming from software serial port ==> data is coming from gpsSerial shield      
 
-        
-    }
-    Serial.write(buffer);
-    Serial.println(" -received-");
+        Serial.write(gpsSerial.read());
+        Serial.println(" -received-");
+    }*/
 
-    gpsSerial.print("valid");
 
-    delay(1000);
+    //delay(10);
+    /*noInterrupts();
+    interrupts();
     //clearBufferArray();
+   */
+    //Serial.println("..");
+    gpsSerial.write(0x4F);
+    delay(100);
     count=0;
 }
 
 void clearBufferArray(){
-    for (int i=0; i<count;i++)
+    for (int i=0; i<maxCnt;i++)
       buffer[i]=NULL; // clear all index of array with command NULL
+}
+
+
+void handleRxInterrupt() {
+  // Cette fonction sera appelée à chaque fois qu'une interruption est déclenchée sur la pin RX
+   // Mettre le drapeau à true pour indiquer que des données ont été reçues
+   if (gpsSerial.available() > 0) {
+      //Serial.println(gpsSerial.read(), HEX);
+      
+       Serial.println(gpsSerial.read(), DEC);
+   }
 }
